efc5774d2ad82069d794e88cbfec93a5c94d95c3
ABDUL QUDOOS <abdulqaudoos786@gmail.com>
2025-09-29 12:52:44 +0500

    cleaner code

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -1,22 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const orderSchema = new mongoose.Schema({
-//   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-//   items: [
-//     {
-//       product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-//       name: String,
-//       qty: Number,
-//       price: Number,
-//       image: String,
-//     },
-//   ],
-//   totalPrice: { type: Number, required: true },
-//   status: { type: String, default: 'Pending' },
-// }, { timestamps: true });
-
-// module.exports = mongoose.model('Order', orderSchema);
-
 
 const mongoose = require('mongoose');
 
